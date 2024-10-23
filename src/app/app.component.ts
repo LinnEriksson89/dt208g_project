61601638f18e3f776c278d6d9c77c267dc16978d
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-10-23 22:08:04 +0200

    adding favicon, reuse of partials as first dragt etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'project';
+  title = 'Project';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './views/start/start.component';
+import { CoursesComponent } from './views/courses/courses.component';
+import { ScheduleComponent } from './views/schedule/schedule.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "index", title: "Start", component: StartComponent },
+    { path: "", redirectTo: "index", pathMatch: "full" },
+    { path: "courses", title: "Kurser", component: CoursesComponent },
+    { path: "schedule", title: "Ramschema", component: ScheduleComponent },
+    { path: "**", title: "404", component: NotFoundComponent }
+];
