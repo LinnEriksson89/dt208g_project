654dd0e430d8d07e6335eb73f2052cf0478dfd4a
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-10-25 16:57:09 +0200

    Small changes to social icons and adding of placeholder-text etc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { StartComponent } from './views/start/start.component';
+import { AboutComponent } from './views/about/about.component';
 import { CoursesComponent } from './views/courses/courses.component';
 import { ScheduleComponent } from './views/schedule/schedule.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
@@ -7,6 +8,7 @@ import { NotFoundComponent } from './views/not-found/not-found.component';
 export const routes: Routes = [
     { path: "index", title: "Start", component: StartComponent },
     { path: "", redirectTo: "index", pathMatch: "full" },
+    { path: "about", title: "Om oss", component: AboutComponent },
     { path: "courses", title: "Kurser", component: CoursesComponent },
     { path: "schedule", title: "Ramschema", component: ScheduleComponent },
     { path: "**", title: "404", component: NotFoundComponent }
