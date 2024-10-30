a6c9c510c7f22c970264eed64492f7329a58191f
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-10-30 18:28:24 +0100

    this is also part of paginator

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { CustomPaginator } from './services/customPaginatorConfiguration';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), {provide: MatPaginatorIntl, useValue: CustomPaginator}]
 };
